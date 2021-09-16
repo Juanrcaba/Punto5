99d45257d11f760204b0564a898beecb86ae2e0a
Juan Santos <juanrscaba@gmail.com>
2021-09-16 13:44:06 -0400

    Add project files.

---
+++ Punto5/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Punto5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txt_ncuenta = new System.Windows.Forms.TextBox();
+            this.txt_cuenta = new System.Windows.Forms.TextBox();
+            this.cmb_tipo = new System.Windows.Forms.ComboBox();
+            this.txt_monto = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dgbDatos = new System.Windows.Forms.DataGridView();
+            this.txt_Debito = new System.Windows.Forms.TextBox();
+            this.txt_Credito = new System.Windows.Forms.TextBox();
+            this.Cuenta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NCuenta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.debito = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.credito = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.msg_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgbDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_ncuenta
+            // 
+            this.txt_ncuenta.Location = new System.Drawing.Point(118, 46);
+            this.txt_ncuenta.Name = "txt_ncuenta";
+            this.txt_ncuenta.Size = new System.Drawing.Size(328, 20);
+            this.txt_ncuenta.TabIndex = 0;
+            // 
+            // txt_cuenta
+            // 
+            this.txt_cuenta.Location = new System.Drawing.Point(12, 46);
+            this.txt_cuenta.Name = "txt_cuenta";
+            this.txt_cuenta.Size = new System.Drawing.Size(100, 20);
+            this.txt_cuenta.TabIndex = 0;
+            // 
+            // cmb_tipo
+            // 
+            this.cmb_tipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_tipo.FormattingEnabled = true;
+            this.cmb_tipo.Items.AddRange(new object[] {
+            "Debito",
+            "Credito"});
+            this.cmb_tipo.Location = new System.Drawing.Point(452, 45);
+            this.cmb_tipo.Name = "cmb_tipo";
+            this.cmb_tipo.Size = new System.Drawing.Size(121, 21);
+            this.cmb_tipo.TabIndex = 1;
+            // 
+            // txt_monto
+            // 
+            this.txt_monto.Location = new System.Drawing.Point(579, 46);
+            this.txt_monto.Name = "txt_monto";
+            this.txt_monto.Size = new System.Drawing.Size(100, 20);
+            this.txt_monto.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(685, 46);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // dgbDatos
+            // 
+            this.dgbDatos.AllowUserToAddRows = false;
+            this.dgbDatos.AllowUserToDeleteRows = false;
+            this.dgbDatos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgbDatos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgbDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgbDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Cuenta,
+            this.NCuenta,
+            this.debito,
+            this.credito});
+            this.dgbDatos.Location = new System.Drawing.Point(12, 72);
+            this.dgbDatos.Name = "dgbDatos";
+            this.dgbDatos.ReadOnly = true;
+            this.dgbDatos.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            this.dgbDatos.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgbDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgbDatos.Size = new System.Drawing.Size(748, 255);
+            this.dgbDatos.TabIndex = 4;
+            // 
+            // txt_Debito
+            // 
+            this.txt_Debito.Enabled = false;
+            this.txt_Debito.Location = new System.Drawing.Point(554, 346);
+            this.txt_Debito.Name = "txt_Debito";
+            this.txt_Debito.Size = new System.Drawing.Size(100, 20);
+            this.txt_Debito.TabIndex = 5;
+            // 
+            // txt_Credito
+            // 
+            this.txt_Credito.Enabled = false;
+            this.txt_Credito.Location = new System.Drawing.Point(660, 346);
+            this.txt_Credito.Name = "txt_Credito";
+            this.txt_Credito.Size = new System.Drawing.Size(100, 20);
+            this.txt_Credito.TabIndex = 6;
+            // 
+            // Cuenta
+            // 
+            this.Cuenta.HeaderText = "Cuenta";
+            this.Cuenta.Name = "Cuenta";
+            this.Cuenta.ReadOnly = true;
+            // 
+            // NCuenta
+            // 
+            this.NCuenta.HeaderText = "Nombre Cuenta";
+            this.NCuenta.Name = "NCuenta";
+            this.NCuenta.ReadOnly = true;
+            // 
+            // debito
+            // 
+            this.debito.HeaderText = "Debito";
+            this.debito.Name = "debito";
+            this.debito.ReadOnly = true;
+            // 
+            // credito
+            // 
+            this.credito.HeaderText = "Credito";
+            this.credito.Name = "credito";
+            this.credito.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Cuenta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nombre Cuenta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(449, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tipo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(582, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Monto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(551, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Debito";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(657, 330);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Credito";
+            // 
+            // msg_label
+            // 
+            this.msg_label.AutoSize = true;
+            this.msg_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msg_label.ForeColor = System.Drawing.Color.Blue;
+            this.msg_label.Location = new System.Drawing.Point(316, 347);
+            this.msg_label.Name = "msg_label";
+            this.msg_label.Size = new System.Drawing.Size(203, 16);
+            this.msg_label.TabIndex = 7;
+            this.msg_label.Text = "Cumple con la partida doble";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 382);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.msg_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Credito);
+            this.Controls.Add(this.txt_Debito);
+            this.Controls.Add(this.dgbDatos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txt_monto);
+            this.Controls.Add(this.cmb_tipo);
+            this.Controls.Add(this.txt_cuenta);
+            this.Controls.Add(this.txt_ncuenta);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Entrada de diario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgbDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_ncuenta;
+        private System.Windows.Forms.TextBox txt_cuenta;
+        private System.Windows.Forms.ComboBox cmb_tipo;
+        private System.Windows.Forms.TextBox txt_monto;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgbDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cuenta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NCuenta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn debito;
+        private System.Windows.Forms.DataGridViewTextBoxColumn credito;
+        private System.Windows.Forms.TextBox txt_Debito;
+        private System.Windows.Forms.TextBox txt_Credito;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label msg_label;
+    }
+}
+
---
+++ Punto5/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //declaramos las variables a utilizar en el programa
+        double _credito = 0, _debito = 0, auxiliar = 0;
+
+        //evento click del boton agregar
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //evaluamos si los campos estan vacios, de no estarlo entonces se procedera a ejecutar el codigo dentro de la condicion
+            if (txt_cuenta.Text != string.Empty && txt_ncuenta.Text != string.Empty && txt_monto.Text != string.Empty && cmb_tipo.Text != string.Empty)
+            {
+                //evaluamos si es a debito o a credito que se agregara el valor
+                if (cmb_tipo.Text.ToLower() == "debito")
+                    dgbDatos.Rows.Add(txt_cuenta.Text, txt_ncuenta.Text, txt_monto.Text, "-");
+                else
+                    dgbDatos.Rows.Add(txt_cuenta.Text, txt_ncuenta.Text, "-", txt_monto.Text);
+
+                Limpiar(); //limpiamos los textbox con la funcion limpiar creada mas abajo
+                calculo(dgbDatos); //ejecutamos la funcion que hara el calulo y mostrara en los textbox correspondientes el saldo en debito y credito
+
+                //codigo para simular la partida doble de la contabilidad, si los montos son diferentes, la partida doble no se cumple
+                if (txt_Debito.Text != txt_Credito.Text)
+                {
+                    msg_label.Text = "No cumple con la partida doble";
+                    msg_label.ForeColor = Color.Red;
+                }
+                else
+                {
+                    msg_label.Text = "Cumple con la partida doble";
+                    msg_label.ForeColor = Color.Blue;
+                }
+            }
+            else
+            {
+                //si falta algun dato en los textbox entonces arrojara este error en pantalla
+                MessageBox.Show("Debe completar todos los campos");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmb_tipo.SelectedIndex = 0; //inicia el combo box en la posicion 0 
+        }
+
+        //funcion para limpiar los componentes textbox
+        void Limpiar(){
+            txt_cuenta.Clear();
+            txt_ncuenta.Clear();
+            txt_monto.Clear();
+            cmb_tipo.SelectedIndex = 0;
+        }
+
+        //funcion para calcular los montos en los textbox de debito y credito
+        void calculo(DataGridView dgdatos)
+        {
+            foreach (DataGridViewRow row in dgdatos.Rows)
+            {
+                if (row.Cells[2].Value.ToString() != "-")
+                {
+                    auxiliar = Convert.ToDouble(row.Cells[2].Value);
+                    _debito += auxiliar;
+                }
+                if (row.Cells[3].Value.ToString() != "-")
+                {
+                    auxiliar = Convert.ToDouble(row.Cells[3].Value);
+                    _credito += auxiliar;
+                }        
+
+
+            }
+            
+            txt_Credito.Text = _credito.ToString("#,###.##");
+            txt_Debito.Text = _debito.ToString("#,###.##");
+            auxiliar = 0;
+            _debito = 0;
+            _credito = 0;
+        }
+    }
+}
